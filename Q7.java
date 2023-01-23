d77ed63e3bf01a4b14c54a29018df89069ce1b24
adiroy2 <adityaroy2.ar.ar@gmail.com>
2023-01-24 00:04:45 +0530

    merged Q7

---
+++ Q7.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+import java.util.*;
+public class Q7{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        System.out.println(Math.max(a, Math.max(b, c)));
+    }
+        
+
+    
+}
