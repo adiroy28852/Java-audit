2da241c635a6cd06b3cf619f68c3804ff1a1e5f9
adiroy2 <adityaroy2.ar.ar@gmail.com>
2023-01-23 22:37:37 +0530

    Week 1 lab in progress

---
+++ week-1/Q9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Q9{
+   public static void main(String[] args){
+      int res=0;
+      for(int i=0; i<31; i++){
+         if (i%10!=0)res+=i;
+      }
+      System.out.println(res);
+        
+   }
+
+    
+}
